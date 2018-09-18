779d8d6af0ae0c999e189e61953f25860ee511be
schmidtster <41802830+schmidtster@users.noreply.github.com>
2018-09-18 16:35:52 -0400

    Update magic_eight.py

--- magic_eight.py
+++ magic_eight.py
@@ -1,1 +1,11 @@
+import random
+
 eight_ball_input = input("What is your question?")
+
+list_of_responses = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes - definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."]
+
+num = random.randint(1,20)
+
+response = list_of_responses[num]
+
+print(response)
